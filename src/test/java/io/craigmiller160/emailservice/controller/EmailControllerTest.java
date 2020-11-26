8f200c9df2e2b8b5feeb790bd4ecab29baa530c3
Craig Miller <craigmiller160@gmail.com>
2020-11-26 14:46:39 -0500

    Got tests to run

--- src/test/java/io/craigmiller160/emailservice/controller/EmailControllerTest.java
+++ src/test/java/io/craigmiller160/emailservice/controller/EmailControllerTest.java
@@ -1,6 +1,8 @@
 package io.craigmiller160.emailservice.controller;
 
+import io.craigmiller160.emailservice.config.EmailConfig;
 import io.craigmiller160.emailservice.email.EmailService;
+import io.craigmiller160.oauth2.config.OAuthConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 public class EmailControllerTest {
 
+    @MockBean
+    private OAuthConfig oAuthConfig;
+    @MockBean
+    private EmailConfig emailConfig;
     @MockBean
     private EmailService emailService;
 
