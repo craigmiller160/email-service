7fb93e872767abea67b007a29e9f493d380433a3
Craig Miller <craigmiller160@gmail.com>
2020-11-26 14:08:20 -0500

    Built more logic for email service

--- src/main/java/io/craigmiller160/emailservice/config/EmailConfig.java
+++ src/main/java/io/craigmiller160/emailservice/config/EmailConfig.java
@@ -1,7 +1,18 @@
 package io.craigmiller160.emailservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class EmailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        final var mailSender = new JavaMailSenderImpl();
+        // TODO configure this
+        return mailSender;
+    }
+
 }
--- src/main/java/io/craigmiller160/emailservice/email/EmailService.java
+++ src/main/java/io/craigmiller160/emailservice/email/EmailService.java
@@ -1,7 +1,22 @@
 package io.craigmiller160.emailservice.email;
 
+import io.craigmiller160.emailservice.dto.EmailRequest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(final JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(final EmailRequest emailRequest) {
+        final var message = new SimpleMailMessage();
+        // TODO finish this
+    }
+
 }
