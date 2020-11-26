7f3931663a468f51515d0d640159bdac4a95b0af
Craig Miller <craigmiller160@gmail.com>
2020-11-26 17:36:02 -0500

    Added logging to EmailService

--- src/main/java/io/craigmiller160/emailservice/email/EmailService.java
+++ src/main/java/io/craigmiller160/emailservice/email/EmailService.java
@@ -4,14 +4,16 @@ import io.craigmiller160.emailservice.dto.EmailRequest;
 import io.craigmiller160.emailservice.exception.SendEmailException;
 import io.vavr.control.Option;
 import io.vavr.control.Try;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-
 @Service
 public class EmailService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
 
     private final JavaMailSender javaMailSender;
 
@@ -20,6 +22,13 @@ public class EmailService {
     }
 
     public Try<Void> sendEmail(final EmailRequest emailRequest) {
+        log.info("Preparing to send email");
+        log.debug("To: {}, CC: {}, BCC: {}, Subject: {}",
+                emailRequest.toAddresses(),
+                emailRequest.ccAddresses(),
+                emailRequest.bccAddresses(),
+                emailRequest.subject()
+        );
         return Try.run(() -> {
             final var message = new SimpleMailMessage();
             Option.of(emailRequest.toAddresses())
@@ -43,6 +52,7 @@ public class EmailService {
             message.setSubject(emailRequest.subject());
             message.setText(emailRequest.text());
             javaMailSender.send(message);
+            log.info("Email sent successfully");
         })
                 .recoverWith(ex -> Try.failure(new SendEmailException("Error sending email", ex)));
     }
