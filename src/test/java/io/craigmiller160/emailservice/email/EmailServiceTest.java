c2a1277c478ad2ba361d9508cfb386d78ea318d8
Craig Miller <craigmiller160@gmail.com>
2020-11-26 13:59:54 -0500

    Stubbed out tests

---
+++ src/test/java/io/craigmiller160/emailservice/email/EmailServiceTest.java
@@ -0,0 +1,15 @@
+package io.craigmiller160.emailservice.email;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceTest {
+
+    @Test
+    public void test() {
+        throw new RuntimeException();
+    }
+
+}
