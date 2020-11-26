d7a3dd44428e9c5f234ab9519464a51495ff2b18
Craig Miller <craigmiller160@gmail.com>
2020-11-26 14:43:22 -0500

    More test enhancements

--- src/test/java/io/craigmiller160/emailservice/controller/EmailControllerTest.java
+++ src/test/java/io/craigmiller160/emailservice/controller/EmailControllerTest.java
@@ -1,16 +1,26 @@
 package io.craigmiller160.emailservice.controller;
 
+import io.craigmiller160.emailservice.email.EmailService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class EmailControllerTest {
 
+    @MockBean
+    private EmailService emailService;
+
     @Test
-    public void test() {
+    public void test_sendEmail() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_sendEmail_unauthorized() {
         throw new RuntimeException();
     }
 
