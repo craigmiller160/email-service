a10ee7aacae592a16acf0204bf479cad85167a12
Craig Miller <craigmiller160@gmail.com>
2020-11-26 13:47:20 -0500

    Integrated TlsConfigurer

--- src/main/java/io/craigmiller160/emailservice/EmailServiceApplication.java
+++ src/main/java/io/craigmiller160/emailservice/EmailServiceApplication.java
@@ -1,12 +1,18 @@
 package io.craigmiller160.emailservice;
 
+import io.craigmiller160.webutils.tls.TlsConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmailServiceApplication {
 
+    private static final String TRUST_STORE_TYPE = "JKS";
+    private static final String TRUST_STORE_PATH = "truststore.jks";
+    private static final String TRUST_STORE_PASSWORD = "changeit";
+
     public static void main(String[] args) {
+        TlsConfigurer.INSTANCE.configureTlsTrustStore(TRUST_STORE_PATH, TRUST_STORE_TYPE, TRUST_STORE_PASSWORD);
         SpringApplication.run(EmailServiceApplication.class, args);
     }
 
