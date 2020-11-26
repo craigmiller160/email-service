2c81bc5f09f6a34e0e558602b44f3e2780e00dd8
Craig Miller <craigmiller160@gmail.com>
2020-11-26 13:19:48 -0500

    Initial commit

---
+++ src/test/java/io/craigmiller160/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.craigmiller160.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
