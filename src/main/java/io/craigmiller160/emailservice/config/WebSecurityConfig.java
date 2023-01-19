4c7f866890c924884cb6f56ff1d35cfef17f3443
Craig Miller <craigmiller160@gmail.com>
2023-01-19 16:09:49 -0500

    Updating security config

--- src/main/java/io/craigmiller160/emailservice/config/WebSecurityConfig.java
+++ src/main/java/io/craigmiller160/emailservice/config/WebSecurityConfig.java
@@ -1,17 +1,41 @@
 package io.craigmiller160.emailservice.config;
 
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
 
     @Override
     public void configure(final HttpSecurity http) throws Exception {
-        throw new Exception();
+        super.configure(http);
+        http
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/health", "/v3/api-docs", "/v3/api-docs/*", "/swagger-ui/*")
+                .permitAll()
+                .antMatchers("/**")
+                .hasAnyRole("access");
     }
 
 }
