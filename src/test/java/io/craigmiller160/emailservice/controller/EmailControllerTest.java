b6540c28375775b0d59aaf877b6134e42e7df203
Craig Miller <craigmiller160@gmail.com>
2023-01-19 16:24:08 -0500

    Preparing to fix tests

--- src/test/java/io/craigmiller160/emailservice/controller/EmailControllerTest.java
+++ src/test/java/io/craigmiller160/emailservice/controller/EmailControllerTest.java
@@ -15,7 +15,6 @@ import io.craigmiller160.emailservice.testutils.JwtUtils;
 import io.vavr.control.Try;
 import java.security.KeyPair;
 import java.util.List;
-import org.apache.catalina.filters.RestCsrfPreventionFilter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,7 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -51,8 +49,6 @@ public class EmailControllerTest {
 
   @Autowired private MockMvc mockMvc;
   @Autowired private ObjectMapper objectMapper;
-
-  @MockBean private FilterRegistrationBean<RestCsrfPreventionFilter> csrfFilter;
 
   private ApiTestProcessor apiProcessor;
   private String token;
